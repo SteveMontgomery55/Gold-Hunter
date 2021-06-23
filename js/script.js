f4e67714811dda91b54f04acd73945e9abbcca5b
SteveMontgomery55 <stevemontgomeryuk@yahoo.co.uk>
2021-06-23 18:46:11 +0000

    add javascript

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    //Picking two cards
+
+    if (!flippedCard) {
+        flippedCard = true;
+        firstCard = this;
+    } else {
+        flippedCard = false;
+        secondCard = this;
+
+        //Is it a match?
+
+        if (firstCard.dataset.image === secondCard.dataset.image) {
+            setTimeout(() => {
+                firstCard.removeEventListener('click', flipCard);
+                secondCard.removeEventListener('click', flipCard);
+                alert("Congratulations! The money is yours!");
+            }, 1500);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                alert("Sorry, no money for you!");
+            }, 1500);
+        }
+    }
+}
+
+//Making the images random
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
